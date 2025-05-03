7492d83eb94022a1428e40d848a3d911c8f3ef3d
Professor1406 <junrel.sapanta@icloud.com>
2025-05-04 05:53:12 +0800

    Update api.js

--- my-website 0/public/js/api.js
+++ my-website 0/public/js/api.js
@@ -1,5 +1,5 @@
 // And here is the API
-let api_key = " -- Enter your API key -- ";
+let api_key = "648c004c97b5a1425c702528ab88ddac";
 
 // The data coming from the API
 let img_url = "https://image.tmdb.org/t/p/w500";
