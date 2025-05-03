9c3539a511a75475e073b9c548f9ba5fcb498edf
Professor1406 <junrel.sapanta@icloud.com>
2025-05-04 04:54:11 +0800

    Update index.js

--- my-website 3/index.js
+++ my-website 3/index.js
@@ -1,16 +1,14 @@
 // Consts
 const apikey = "648c004c97b5a1425c702528ab88ddac";
-const apiEndpoint = "https://api.themoviedb.org/3"
+const apiEndpoint = "https://api.themoviedb.org/3";
 const imgPath = "https://image.tmdb.org/t/p/original";
-
 
 const apiPaths = {
     fetchAllCategories: `${apiEndpoint}/genre/movie/list?api_key=${apikey}`,
     fetchMoviesList: (id) => `${apiEndpoint}/discover/movie?api_key=${apikey}&with_genres=${id}`,
     fetchTrending:`${apiEndpoint}/trending/all/day?api_key=${apikey}&language=en-US`,
     searchOnYoutube: (query) => `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&key=AIzaSyA_eZ5WJhmYhRQOM8-jAyVIzzdfWUlp_P0`
-}
-
+};
 
 // Boots up the app
 function init() {
@@ -18,17 +16,17 @@ function init() {
     fetchAndBuildAllSections();
 }
 
-function fetchTrendingMovies(){
+function fetchTrendingMovies() {
     fetchAndbuildMovieSection(apiPaths.fetchTrending, 'Trending Now')
     .then(list => {
         const randomIndex = parseInt(Math.random() * list.length);
         buildBannerSection(list[randomIndex]);
-    }).catch(err=>{
+    }).catch(err => {
         console.error(err);
     });
 }
 
-function buildBannerSection(movie){
+function buildBannerSection(movie) {
     const bannerCont = document.getElementById('banner-section');
     
     bannerCont.style.backgroundImage = `url('${imgPath}${movie.backdrop_path}')`;
@@ -40,8 +38,8 @@ function buildBannerSection(movie){
             <p class="banner__info">Trending in movies | Released - ${movie.release_date} </p>
             <p class="banner__overview">${movie.overview && movie.overview.length > 200 ? movie.overview.slice(0,200).trim()+ '...':movie.overview}</p>
             <div class="action-buttons-cont">
-                <button class="action-button"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="Hawkins-Icon Hawkins-Icon-Standard"><path d="M4 2.69127C4 1.93067 4.81547 1.44851 5.48192 1.81506L22.4069 11.1238C23.0977 11.5037 23.0977 12.4963 22.4069 12.8762L5.48192 22.1849C4.81546 22.5515 4 22.0693 4 21.3087V2.69127Z" fill="currentColor"></path></svg> &nbsp;&nbsp; Play</button>
-                <button class="action-button"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="Hawkins-Icon Hawkins-Icon-Standard"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3ZM1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12ZM13 10V18H11V10H13ZM12 8.5C12.8284 8.5 13.5 7.82843 13.5 7C13.5 6.17157 12.8284 5.5 12 5.5C11.1716 5.5 10.5 6.17157 10.5 7C10.5 7.82843 11.1716 8.5 12 8.5Z" fill="currentColor"></path></svg> &nbsp;&nbsp; More Info</button>
+                <button class="action-button" onclick="playMovie('${movie.title}', '${movie.id}')">Play</button>
+                <button class="action-button" onclick="showMoreInfo('${movie.title}')">More Info</button>
             </div>
         `;
     div.className = "banner-content container";
@@ -49,8 +47,44 @@ function buildBannerSection(movie){
     bannerCont.append(div);
 }
 
+function playMovie(movieName, movieId) {
+    // Fetch video sources from MegaCloud or VidSrc
+    fetchVideoSource(movieId)
+    .then(videoUrl => {
+        const videoPlayer = document.getElementById('videoPlayer');
+        videoPlayer.src = videoUrl;
+        videoPlayer.play();
+    }).catch(err => {
+        console.error("Error fetching video source:", err);
+    });
+}
 
-function fetchAndBuildAllSections(){
+function fetchVideoSource(movieId) {
+    // Example URLs for MegaCloud and VidSrc. Replace with actual fetching logic.
+    const megaCloudUrl = `https://mega.nz/file/${movieId}#yourMegaCloudKey`; // Replace with actual MegaCloud URL
+    const vidSrcUrl = `https://vidsrc.me/video/${movieId}`; // Replace with actual VidSrc URL
+
+    // Check if MegaCloud is accessible first
+    return new Promise((resolve, reject) => {
+        fetch(megaCloudUrl, { method: 'HEAD' })
+        .then(response => {
+            if (response.ok) {
+                resolve(megaCloudUrl); // If MegaCloud is available
+            } else {
+                // Fallback to VidSrc if MegaCloud is unavailable
+                reject('MegaCloud failed, falling back to VidSrc.');
+            }
+        })
+        .catch(() => {
+            reject('MegaCloud failed, falling back to VidSrc.');
+        });
+    })
+    .catch(() => {
+        return Promise.resolve(vidSrcUrl); // Fallback to VidSrc if MegaCloud fails
+    });
+}
+
+function fetchAndBuildAllSections() {
     fetch(apiPaths.fetchAllCategories)
     .then(res => res.json())
     .then(res => {
@@ -63,29 +97,24 @@ function fetchAndBuildAllSections(){
                 );
             });
         }
-        // console.table(movies);
     })
-    .catch(err=>console.error(err));
+    .catch(err => console.error(err));
 }
 
-function fetchAndbuildMovieSection(fetchUrl, categoryName){
-    console.log(fetchUrl,categoryName);
+function fetchAndbuildMovieSection(fetchUrl, categoryName) {
     return fetch(fetchUrl)
     .then(res => res.json())
     .then(res => {
-        // console.table(res.results);
         const movies = res.results;
         if (Array.isArray(movies) && movies.length) {
             buildMoviesSection(movies.slice(0,6), categoryName);
         }
         return movies;
     })
-    .catch(err=>console.error(err))
+    .catch(err => console.error(err));
 }
 
-function buildMoviesSection(list, categoryName){
-    console.log(list, categoryName);
-
+function buildMoviesSection(list, categoryName) {
     const moviesCont = document.getElementById('movies-cont');
     
     const moviesListHTML = list.map(item => {
@@ -101,13 +130,13 @@ function buildMoviesSection(list, categoryName){
         <div class="movies-row">
             ${moviesListHTML}
         </div>
-    `
+    `;
 
     const div = document.createElement('div');
-    div.className = "movies-section"
+    div.className = "movies-section";
     div.innerHTML = moviesSectionHTML;
 
-    // append html into movies container
+    // Append to movies container
     moviesCont.append(div);
 }
 
@@ -120,24 +149,19 @@ function searchMovieTrailer(movieName, iframId) {
         const bestResult = res.items[0];
         
         const elements = document.getElementById(iframId);
-        console.log(elements, iframId);
-
         const div = document.createElement('div');
-        div.innerHTML = `<iframe width="245px" height="150px" src="https://www.youtube.com/embed/${bestResult.id.videoId}?autoplay=1&controls=0"></iframe>`
-// window.open(youtubeUrl,'_blank');
+        div.innerHTML = `<iframe width="245px" height="150px" src="https://www.youtube.com/embed/${bestResult.id.videoId}?autoplay=1&controls=0"></iframe>`;
         elements.append(div);
-        
     })
-    .catch(err=>console.log(err));
+    .catch(err => console.log(err));
 }
 
-
-window.addEventListener('load',function() {
+window.addEventListener('load', function() {
     init();
-    window.addEventListener('scroll', function(){
-        // header ui update
+    window.addEventListener('scroll', function() {
+        // Header UI update
         const header = document.getElementById('header');
-        if (window.scrollY > 5) header.classList.add('black-bg')
+        if (window.scrollY > 5) header.classList.add('black-bg');
         else header.classList.remove('black-bg');
-    })
-})
+    });
+});
