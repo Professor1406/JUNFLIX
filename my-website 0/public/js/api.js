30aed6c82cd04050d2331b442d59b95083a2009c
Professor1406 <junrel.sapanta@icloud.com>
2025-05-04 15:13:21 +0800

    Delete my-website 0 directory

--- my-website 0/public/js/api.js
+++
@@ -1,9 +0,0 @@
-// And here is the API
-let api_key = "648c004c97b5a1425c702528ab88ddac";
-
-// The data coming from the API
-let img_url = "https://image.tmdb.org/t/p/w500";
-let original_img_url = "https://image.tmdb.org/t/p/original";
-let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
-let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
-let movie_detail_http = "https://api.themoviedb.org/3/movie";
